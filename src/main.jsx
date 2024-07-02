7216980aeb2570a1ce679ef1b1a49b360d3d170b
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-07-02 23:19:15 +0200

    Creation of the router and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles.css';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import { HeroesApp } from './HeroesApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HeroesApp />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
+  </React.StrictMode>
 )
