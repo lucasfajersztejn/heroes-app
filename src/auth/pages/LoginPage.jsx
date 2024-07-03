4676f99b1467770f0c4c8806e5ef1367c4f1538a
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-07-03 12:49:50 +0200

    Adding data, helpers and new component HeroList

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,28 @@
+import { useNavigate } from "react-router"
 
 
 export const LoginPage = () => {
+
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    navigate('/', {
+      replace: true
+    })
+  }
+
   return (
-    <>
-      <h1>LoginPage</h1>
-    </>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button 
+        className="btn btn-success"
+        onClick={ onLogin }
+      >
+        Login
+      </button>
+    
+    </div>
   )
 }
--- src/heroes/index.js
+++ src/heroes/index.js
@@ -2,3 +2,4 @@
 
 export * from './pages'
 export * from './routes/HeroesRoutes';
+export * from './components';
