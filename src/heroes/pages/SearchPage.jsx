39affb6a39ba8746968e9c0a5699639716ecd08c
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-07-08 22:54:33 +0200

    Working con SearchPage component

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,9 +1,51 @@
+import { HeroCard } from '../components';
 
 
 export const SearchPage = () => {
   return (
     <>
       <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            >
+            </input>
+          
+            <button className="btn btn-outline-success mt-1">
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-success">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+
+
+          {/* <HeroCard {...hero} /> */}
+        </div>
+
+      </div>
+
+
     </>
   )
 }
