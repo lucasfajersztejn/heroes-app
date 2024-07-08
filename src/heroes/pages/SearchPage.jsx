fe1ad1e66b1b30f1233ea5949952a307a6e9a2a8
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-07-08 23:42:14 +0200

    SearchPage working

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,6 +13,8 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse( location.search );
   const heroes = getHeroesByName( q );
 
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
 
   const {searchText, onInputChange, onResetForm,} = useForm({
     searchText: q,
@@ -21,7 +23,7 @@ export const SearchPage = () => {
   const onSearchSubmit = ( event ) => {
     event.preventDefault();
 
-    if ( searchText.trim().length <= 1 ) return;
+    // if ( searchText.trim().length <= 1 ) return;
 
     navigate(`?q=${ searchText }`)
   }
@@ -57,13 +59,24 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          
+          {/* {
+            ( q === '' )
+              ? <div className="alert alert-success"> Search a hero </div>
+              : ( heroes.length === 0 )
+                &&  <div className="alert alert-danger"> No hero with <b>{ q }</b></div>
+          } */}
 
-          <div className="alert alert-success">
+          <div 
+            className="alert alert-success animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}
+          >
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+          <div 
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}
+          >
             No hero with <b>{ q }</b>
           </div>
 
